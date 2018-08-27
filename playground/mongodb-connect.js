4627f31d07c4d5433152a1fef976ba13945a6fa4
Ron <ronski6200@gmaail.com>
2018-08-27 16:38:13 +0800

    Init Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,57 @@
+// const MongoClient = require('mongodb').MongoClient; // pull-off the MongoClient from the mongodb library
+
+const {MongoClient, ObjectID} = require('mongodb'); // this is object destructuring
+
+var obj = new ObjectID();
+console.log(obj);
+
+// var user = {name: 'Ron Reyes', age: 20}; // object destructuring
+// var {name} = user;
+// console.log(name);
+
+// it has 2 argu. 1 string it might be the url where the db is store, 2 is a callback func
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {  //to connect to the database, TodoApp is the new db. err if an error is exist or not, client object is referiing to client.db('TodoApp')  
+     if (err) {
+         return console.log('Unable to connect to MongDB server.'); // return is use to prevent executing the next code w/c is success
+     } 
+     console.log('Connected to MongoDB server');
+     const db = client.db('TodoApp') // access referencing to the database
+
+    // db.collection('Todos').insertOne({ // this is a collection or table. 2 param. 1 is object to store key value pairs, 2 is callback for either successful or failed 
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2)); // result.ops=> store all the docs that where inserted
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Ron Reyes',
+    //     age: 20,
+    //     location: 'Philippines'
+    // }, (err, result) => { // this is the callback func
+    //     if (err) {
+    //         return console.log('Unable to insert User' , err);
+    //     };
+    //     console.log(result.ops[0]._id.getTimestamp()); // this print the timestamp
+    // });
+
+    client.close(); // this close the db connection
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ playground/mongodb-find.js
@@ -0,0 +1,58 @@
+
+const {MongoClient, ObjectID} = require('mongodb'); // this is object destructuring
+
+
+// it has 2 argu. 1 string it might be the url where the db is store, 2 is a callback func
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {  //to connect to the database, TodoApp is the new db. err if an error is exist or not, client object is referiing to client.db('TodoApp')  
+     if (err) {
+         return console.log('Unable to connect to MongDB server.'); // return is use to prevent executing the next code w/c is success
+     } 
+     console.log('Connected to MongoDB server');
+     const db = client.db('TodoApp') // access referencing to the database
+
+    // ** THIS RETRIEVE BY ID, TEXT OR COMPLETED USING .toArray()
+     db.collection('Todos').find({  // mongodb returns a cursor w/c is toArray, then toArray return a promise w/c is the docs or records and display it in a callback
+         //_id: new ObjectID('5b837dfc17bd4b9a344b8d6f') // retrievin by objectID
+         text: 'Something to do'  // retrieving by text
+        // completed: true  // retrieving by completed
+        }).toArray().then((docs) => {// find{} is the query argu
+        console.log('Todos');
+        console.log(JSON.stringify(docs,undefined,2)); 
+     }, (err) => {
+         console.log('Unable to fetch todos', err);
+     }); 
+
+     // ** THIS DISPLAY THE TOTAL COUNT using .count()
+    // db.collection('Todos').find().count().then((count) => {// find{} is the query argu
+    //    console.log(`Todos Count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // }); 
+
+    db.collection('Users').find({
+        name: /.*Anica.*/  // use /.*text.* similar to like
+    }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs,undefined,2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+
+    // client.close(); // this close the db connection
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
